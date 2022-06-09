799ffe5d5c04d0e59a776716d194d73e7596b81d
blackhook <p369c258@163.com>
2022-06-09 12:34:28 +0800

    修正域名正则匹配问题

--- draw.py
+++ draw.py
@@ -8,11 +8,11 @@ point_end = {'x':0,'y':0}
 count = 10 #马赛克模糊程度
 boxs = []
 lst = []
-re_s = []
+re_list = []
 ip_re = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-domain_re = r"(?<=http[s]://)(((((\d{1,2})|(1\d{2})|(2[0-4]\d)|(25[0-5]))\.){3}((\d{1,2})|(1\d{2})|(2[0-4]\d)|(25[0-5]))(:\d{,8})?)|([.\w-]*))((?=/)|(?!/))"
-re_s.append(ip_re)
-re_s.append(domain_re)
+domain_re = r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z]"
+re_list.append(ip_re)
+re_list.append(domain_re)
 
 def mosaci(box,img):
     point_start['x'] = int(box[0][0])
@@ -28,27 +28,27 @@ def mosaci(box,img):
     img[point_end['x']:point_end['y'], point_start['x']: point_start['y']] = quan[:c1,:c2]
     return img
 
-def findstr(fulltext,ts):
+def findbox(fulltext,ts):
     text = fulltext[1][0]
     for t in ts:
-        if str(t) in str(text):
+        if str(text).find(str(t)) > -1:
             box = fulltext[0]
             return box
         else:
             pass
 if __name__ == '__main__':
     ocr = PaddleOCR(use_angle_cls=False, lang="ch")
-    img_path = '0880c08b0110b1b49619.png'
+    img_path = '0880c08b0110b1f4a11c.png'
     result = ocr.ocr(img_path, cls=False)
     img = cv2.imread(img_path)
     for fulltext in result:
-        for i in re_s:
+        for i in re_list:
             match = re.compile(i).search(fulltext[1][0])
             if match is not None:
                 lst.append(match[0])
             else:
                 pass
-        box = findstr(fulltext,lst)
+        box = findbox(fulltext,lst)
         if box is None:
             pass
         else:
